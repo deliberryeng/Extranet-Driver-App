99c870089f1e9cd47b3dd39a9eecc0868a4dbdc0
ab95david <ab95david@gmail.com>
2017-11-21 11:48:51 +0100

    Add icons and setup login

--- app/src/main/java/com/deliberry/driverextranet/LoginActivity.java
+++ app/src/main/java/com/deliberry/driverextranet/LoginActivity.java
@@ -22,46 +22,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
-    @BindView(R.id.tv_email)
-    TextView tv_email;
-
-    @BindView(R.id.btn_submit)
-    Button btn_submit;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createRequest("http://www.google.com");
-            }
-        });
-    }
-
-    private void createRequest(String url) {
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        tv_email.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                tv_email.setText("That didn't work!");
-                Log.e(TAG, error.getMessage());
-            }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
     }
 }
