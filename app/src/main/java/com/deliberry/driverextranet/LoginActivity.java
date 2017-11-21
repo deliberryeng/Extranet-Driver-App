b71767dbff5780041c700050122de7f52cf490a7
ab95david <ab95david@gmail.com>
2017-11-21 12:52:54 +0100

    Update login layout

--- app/src/main/java/com/deliberry/driverextranet/LoginActivity.java
+++ app/src/main/java/com/deliberry/driverextranet/LoginActivity.java
@@ -2,20 +2,6 @@ package com.deliberry.driverextranet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
